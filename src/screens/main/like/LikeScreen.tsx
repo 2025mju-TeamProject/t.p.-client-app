165f22f9aeabc29df0a3adc5d57f07508a8bb0df
sehoon Kim <kimsese1403@gmail.com>
2025-10-18 02:48:28 +0900

    [Chore] 임시 화면 추가 #10

---
+++ src/screens/main/like/LikeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from 'react-native-webview/lib/WebView.styles';
+import { View, Text } from 'react-native';
+
+function LikeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Like</Text>
+    </View>
+  )
+}
+
+export default LikeScreen;
---
+++ src/screens/main/setting/SettingScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from 'react-native-webview/lib/WebView.styles';
+import { View, Text } from 'react-native';
+
+function SettingScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Setting</Text>
+    </View>
+  )
+}
+
+export default SettingScreen;
