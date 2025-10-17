1925789ffd45c762430e99802b8e985e077bfdce
sehoon Kim <kimsese1403@gmail.com>
2025-10-18 02:46:46 +0900

    [Feat] 현재 창에 따라 하단 바 아이콘 색 변경 #10

--- src/navigation/BottomNavigator.tsx
+++ src/navigation/BottomNavigator.tsx
@@ -6,41 +6,54 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import SearchScreen from '../screens/main/SearchScreen';
 import ChatListScreen from '../screens/main/chat/ChatListScreen';
 
-function RootNavigator(){
+function RootNavigator() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        // 현재 route.name 에 따라 탭 아이콘 분기
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: string = "";
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home-sharp' : 'home-outline';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'heart-sharp' : 'heart-outline';
+          } else if (route.name === 'ChatList') {
+            iconName = focused ? 'chatbubble-sharp' : 'chatbubble-outline';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'black', // 활성 상태 색
+        tabBarInactiveTintColor: 'black', // 비활성 색
+      })}
+    >
       <Tab.Screen
         name={ROUTES.HOME}
         component={HomeScreen}
         options={{
           headerShown: false,
-          title: '',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="home-sharp" color={color} size={size} />
-          ),
-        }}/>
+          title: '홈',
+        }}
+      />
       <Tab.Screen
         name={ROUTES.SEARCH}
         component={SearchScreen}
         options={{
-          title: '',
-          tabBarIcon: ({color, size}) => (
-            <Icon name="search" color={color} size={size} />
-          ),
-        }}/>
+          title: '관심',
+        }}
+      />
       <Tab.Screen
         name={ROUTES.CHATLIST}
         component={ChatListScreen}
         options={{
           headerShown: false,
-          tabBarIcon: ({color, size}) => (
-            <Icon name="chatbubble-sharp" color={color} size={size} />
-          ),
-        }}/>
+          title: '채팅',
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 }
 
 export default RootNavigator;
