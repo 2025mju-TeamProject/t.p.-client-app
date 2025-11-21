f3ff4fff051eeaf6ae13497572032f11aae60990
sehoon Kim <kimsese1403@gmail.com>
2025-11-22 07:56:19 +0900

    [Chore] 소개글 작성화면 구현 #18
    - 마지막 페이지 -> login화면

--- src/screens/start/WriteProfileScreen.tsx
+++ src/screens/start/WriteProfileScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import BirthdayScreen from '../../screens/start/write_profile/BirthdayScreen';
 import GenderScreen from '../../screens/start/write_profile/GenderScreen';
 import AppButton from '../../components/buttons/AppButton';
@@ -11,17 +11,27 @@ import JobScreen from '../../screens/start/write_profile/JobScreen';
 import LocationScreen from '../../screens/start/write_profile/LocationScreen';
 import ImageScreen from '../../screens/start/write_profile/ImageScreen';
 import IntroductionScreen from '../../screens/start/write_profile/IntroductionScreen';
+import Modal from 'react-native-modal';
+import ROUTES from '../../constants/routes';
 
-function WriteProfileScreen() {
+const messages = ['쿠피가 프로필 작성 중', '쿠피가 프로필 작성 중.', '쿠피가 프로필 작성 중..', '쿠피가 프로필 작성 중...']
+
+function WriteProfileScreen({ navigation }: any) {
   const pagerRef = useRef<PagerView>(null);
   const [page, setPage] = useState<number>(0);
   const [gender, setGender] = useState<string>('');
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [messageIndex, setMessageIndex] = useState<number>(0);
 
   const totalPages = 8;
 
   function goNext() {
     if (!pagerRef.current) return;
-    if (page >= totalPages - 1) return;
+    if (page > totalPages - 1) return;
+    if (page === totalPages - 1) {
+      handleLogin()
+      return;
+    }
 
     pagerRef.current.setPage(page + 1);
   }
@@ -33,11 +43,46 @@ function WriteProfileScreen() {
     pagerRef.current.setPage(page - 1);
   }
 
+  const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  useEffect(() => {
+    if (page === totalPages - 1) {
+      async function run() {
+        setModalVisible(true);
+        await wait(2000);
+        setModalVisible(false);
+      }
+      run()
+    }
+  }, [page]);
+
+  useEffect(() => {
+    if(modalVisible) {
+      const timer = setInterval(() => {
+        setMessageIndex(prev => (prev + 1) % messages.length);
+      }, 1000); // 1초
+
+    }
+  }, [modalVisible]);
+
+  async function handleLogin(){
+    if (page === totalPages - 1) {
+      setModalVisible(true);
+
+      await wait(3000)
+      setModalVisible(false)
+
+      navigation.reset({
+        index: 0,
+        routes: [{ name: ROUTES.LOGIN }],
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* 헤더 */}
       <View style={styles.header}>
-        {/*<Text style={{ color: 'black' }}>{gender}</Text>*/}
       </View>
 
       {/* 페이지 뷰 */}
@@ -82,9 +127,31 @@ function WriteProfileScreen() {
         </View>
 
         <View key="introduction">
-          <IntroductionScreen />
+          <IntroductionScreen
+            modalVisible={modalVisible} />
         </View>
       </PagerView>
+
+      <Modal
+        isVisible={modalVisible}
+        onBackdropPress={() => {}}
+        animationIn="fadeInUp"
+        animationOut="fadeOutDown"
+        style={{ justifyContent: 'center' }}>
+
+        <View style={styles.copiModal}>
+          <View style={{alignItems: 'center', gap: 10}}>
+            <Image source={require('../../../assets/cupi.png')} style={{width: 110, height: 90}} />
+            <Text style={styles.modalTitle}>{messages[messageIndex]}</Text>
+            <Text style={styles.modalSubtitle}>AI 어시스턴트 쿠피와 함께 프로필을 써봐요!</Text>
+          </View>
+
+          <View style={{justifyContent: 'space-between', gap: 10, marginHorizontal: 24}}>
+            <Text style={{fontSize: 16, fontWeight: 800}}>쿠피는 무슨일을 하나요?</Text>
+            <Text style={{fontSize: 13, fontWeight: 700, color: '#434343'}}>{'\‘쿠피\’는 회원님이 지금까지 입력한 모든 정보인 성별, 생일, 관심사, mbti, 직업, 지역을 기반으로 프로필 소개글을 대신 작성해줘요.'}</Text>
+          </View>
+        </View>
+      </Modal>
 
       {/* 하단 버튼  */}
       <View style={[styles.bottomTab, {gap: 10}]}>
@@ -157,4 +224,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f4f4f4',
   },
+  copiModal: {
+    width: '100%',
+    height: 381,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 18,
+  },
+  modalTitle: {
+    fontSize: 19,
+    fontWeight: 800,
+    color: '#FF4239'
+  },
+  modalSubtitle: {
+    fontSize: 13,
+    fontWeight: 700,
+    color: '#434343'
+  }
 });
--- src/screens/start/write_profile/IntroductionScreen.tsx
+++ src/screens/start/write_profile/IntroductionScreen.tsx
@@ -1,21 +1,68 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { JSX, useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import styles from './writeProfileStyles';
+import Modal from 'react-native-modal';
 
-function IntroductionScreen() {
+type Props = {
+  modalVisible: boolean;
+}
+
+function IntroductionScreen({ modalVisible }: Props) {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(modalVisible);
+  const [introduction, setIntroduction] = useState<string>(
+    ' 안녕하세요, 감자맛탕입니다. 고양이 \n' +
+      '털에 묻은 그래픽 디자이너이며, \n' +
+      "주말엔 캠핑을 즐기는 '갑술' 여자입니다. 갑술의 성향처럼 편안한 사람들과 어울리는 것을 좋아해요. LP판을 들으며 마시는 커피 한 잔의 여유를 아는 그런… 가끔은 말수가 적어도 이해해주실 수 있나요? 그렇다면, 긍정적이고 따뜻한 세상을 함께 만들어가요. 유머가 조금 섞인, 친근한 대화로 시작해볼까요?",
+  );
+
   return (
     <View style={styles.container}>
       <View style={[styles.section, {marginTop: 30}]}>
         <Text style={styles.title}>{'마지막 단계예요.\n프로필 소개글을 써볼까요?'}</Text>
       </View>
       <View style={[styles.section, {marginTop: 5}]}>
-        <Text style={styles.subTitle}>회원님을 소개하는 글을 작서앻 주세요.</Text>
+        <Text style={styles.subTitle}>회원님을 소개하는 글을 작성해 주세요.</Text>
       </View>
 
+      <ScrollView style={{marginTop: 30}}>
+        <View style={styles.section}>
+          <Text style={[styles.boldText, {fontSize: 16, color: '#FF4239'}]}>AI 어시스턴스 쿠피를 소개합니다!</Text>
+        </View>
+        <View style={[styles.section, {marginTop: 10}]}>
+          <Text style={styles.text}>{'\\‘쿠피\\’는 회원님이 지금까지 입력한 모든 정보인 성별, 생일, 관심사, MBTI, 직업, 지역을 기반으로 프로필 소개글을 작성해주는 AI 어시스턴트예요.'}</Text>
+        </View>
+        <View style={[styles.section, {marginTop: 10}]}>
+          <Text style={{fontSize: 14, fontWeight: 700,}}>쿠피가 회원님의 프로필 글을 작성해 봤어요!</Text>
+        </View>
 
-
+        <View style={[styles.section, {marginTop: 10, marginBottom: 30}]}>
+          <TextInput
+            style={localStyles.textInput}
+            multiline
+            value={introduction}
+            onChangeText={setIntroduction}
+            scrollEnabled={false} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 export default IntroductionScreen;
+
+const localStyles = StyleSheet.create({
+  textInput: {
+    width: '100%',
+    height: 262,
+    maxHeight: 'auto',
+    paddingHorizontal: 24,
+    paddingVertical: 24,
+    borderWidth: 1,
+    borderColor: '#EDEDED',
+    borderRadius: 10,
+    fontSize: 14,
+    lineHeight: 24,
+    textAlign: 'left',
+    textAlignVertical: 'top',
+  },
+})
