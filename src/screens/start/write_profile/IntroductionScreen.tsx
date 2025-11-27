95a8319870524fd2f90c663bafb29cb8f2a51c12
SARA-MAYO <seolha.lee423@gmail.com>
2025-11-28 00:01:23 +0900

    feat: 날개를 달아요 훨훨

--- src/screens/start/write_profile/IntroductionScreen.tsx
+++ src/screens/start/write_profile/IntroductionScreen.tsx
@@ -1,7 +1,11 @@
 import React, { JSX, useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+
 import styles from './writeProfileStyles';
 import Modal from 'react-native-modal';
+
+const cupiRight = '../../../../assets/cupi_wings_right.png';
+const cupiLeft = '../../../../assets/cupi_wings_left.png';
 
 type Props = {
   modalVisible: boolean;
@@ -14,47 +18,74 @@ function IntroductionScreen({ modalVisible }: Props) {
     조용하지만 유머코드 잘 맞는 사람, 서로의 속도를 존중하며 편안하게 이야기 나눌 수 있는 분이라면 좋은 인연이 될 수 있을 것 같아요!
     우리, 커피 한 잔처럼 부담 없이 대화부터 시작해볼까요?`);
 
-  return (
-    <View style={styles.container}>
-      <View style={[styles.section, {marginTop: 30}]}>
-        <Text style={styles.title}>{'마지막 단계예요.\n프로필 소개글을 써볼까요?'}</Text>
+    return (
+      <View style={styles.container}>
+
+        {/* 여기 ScrollView 추가!! */}
+        <ScrollView style={{marginTop: 34}}>
+
+          <View style={[styles.section, ]}>
+            <Text style={styles.title}>{'마지막 단계예요.\n프로필 소개글을 써볼까요?'}</Text>
+          </View>
+
+          <View style={[styles.section, {marginTop: 5}]}>
+            <Text style={styles.subTitle}>회원님을 소개하는 글을 작성해 주세요.</Text>
+          </View>
+
+          {/* AI 어시스턴트 쿠피 소개글 - 타이틀 */}
+          <View style={[styles.section, {
+              flexDirection: 'row', alignItems: 'center', gap: 3, marginTop:32 }]}>
+            <Image
+            source={require(cupiLeft)}
+            style={{ width: 20, height: 20, resizeMode: 'contain' }}
+            />
+            <Text
+              style={[
+                styles.boldText,
+                { fontSize: 16, color: '#FB222D', letterSpacing: -0.3 }
+              ]}
+            >
+              AI 어시스턴스 쿠피를 소개합니다
+            </Text>
+            <Image
+              source={require(cupiRight)}
+              style={{ width: 20, height: 20, resizeMode: 'contain' }}
+            />
+          </View>
+
+          {/* AI 어시스턴트 쿠피 소개글 - 설명 */}
+          <View style={[styles.section, { marginTop: 10 }]}>
+            <Text style={[styles.text13, { lineHeight: 20, color: '#515151', letterSpacing: -0.3 }]}>
+              <Text style={{ fontFamily: 'NanumSquareB' }}>쿠피</Text>는 회원님이 지금까지 입력한 모든 정보인
+              '성별, 생일, 관심사, MBTI, 직업, 지역'을 참고해서 프로필 소개글을 작성해주는{' '}
+              <Text style={{ fontFamily: 'NanumSquareB' }}>
+                AI 어시스턴트
+              </Text>
+              예요.
+            </Text>
+          </View>
+
+          <View style={[styles.section, { marginTop: 30 }]}>
+            <Text style={[styles.boldTextBB, { letterSpacing: -0.3 }]}>
+              쿠피가 프로필 글을 작성해 봤어요
+            </Text>
+          </View>
+
+          <View style={[styles.section, {marginTop: 16, marginBottom: 30}]}>
+            <TextInput
+              style={localStyles.textInput}
+              multiline
+              value={introduction}
+              onChangeText={setIntroduction}
+              scrollEnabled={false}
+            />
+          </View>
+
+        </ScrollView>
       </View>
-      <View style={[styles.section, {marginTop: 5}]}>
-        <Text style={styles.subTitle}>회원님을 소개하는 글을 작성해 주세요.</Text>
-      </View>
+    );
+  }
 
-      <ScrollView style={{marginTop: 30}}>
-        <View style={styles.section}>
-          <Text style={[styles.boldText, {fontSize: 16, color: '#FF281D', letterSpacing: -0.3}]}>
-          AI 어시스턴스 쿠피를 소개합니다!</Text>
-        </View>
-
-        <View style={[styles.section, { marginTop: 14 }]}>
-          <Text style={[styles.text13, { lineHeight: 20, color: '#515151', letterSpacing: -0.3 }]}>
-            <Text style={{ fontFamily: 'NanumSquareB' }}>쿠피</Text>는 회원님이 지금까지 입력한 모든 정보인
-            '성별, 생일, 관심사, MBTI, 직업, 지역'을 참고해서 프로필 소개글을 작성해주는{' '}
-            <Text style={{ fontFamily: 'NanumSquareB' }}>
-              AI 어시스턴트</Text>예요.
-          </Text>
-        </View>
-
-
-        <View style={[styles.section, { marginTop: 24, }]}>
-          <Text style={[styles.boldTextBB, { letterSpacing: -0.3 }]}>👇 쿠피가 회원님의 프로필 글을 작성해 봤어요.</Text>
-        </View>
-
-        <View style={[styles.section, {marginTop: 16, marginBottom: 30}]}>
-          <TextInput
-            style={localStyles.textInput}
-            multiline
-            value={introduction}
-            onChangeText={setIntroduction}
-            scrollEnabled={false} />
-        </View>
-      </ScrollView>
-    </View>
-  );
-}
 
 export default IntroductionScreen;
 
