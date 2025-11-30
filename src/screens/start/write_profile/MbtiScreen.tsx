4ab3c32c54f870d293e5b41c2deed4cfad1b8727
sehoon Kim <kimsese1403@gmail.com>
2025-12-01 00:07:58 +0900

    [Chore] MBTI화면 상단 글자 수정 #36

--- src/screens/start/write_profile/MbtiScreen.tsx
+++ src/screens/start/write_profile/MbtiScreen.tsx
@@ -48,7 +48,7 @@ function MbtiScreen({ setParentMbti }: Props) {
   return (
     <View style={styles.container}>
       <View style={[styles.section, { marginTop: 30 }]}>
-        <Text style={styles.title}>{mbti}를 알려주세요</Text>
+        <Text style={styles.title}>MBTI를 알려주세요</Text>
       </View>
       <View style={[styles.section, { marginTop: 5 }]}>
         <Text style={styles.subTitle}>모든 항목을 선택해 주세요.</Text>
